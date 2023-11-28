73cf76c90e5f311b32a64268ead53aae4e2d1441
ousttrue <ousttrue@gmail.com>
2023-11-28 09:06:27 +0900

    WIP react

---
+++ mediasoup/producer_app.ts
@@ -0,0 +1,118 @@
+import * as MediasoupClient from "mediasoup-client";
+import {
+  WebSocketJsonRpc,
+  JsonRpcDispatcher, JsonRpcDispatchEvent
+} from '../ws-json-rpc.js';
+
+
+export class VideoCanvas {
+  constructor(
+    public readonly video: HTMLVideoElement,
+    public readonly canvas: HTMLCanvasElement,
+    public readonly imageFormat = "image/png",
+    public readonly canvasWidth = 320,
+    public readonly canvasHeight = 180,
+  ) { }
+
+  async waitButton(buttonStart: HTMLButtonElement): Promise<void> {
+    return new Promise((resolve) => {
+      buttonStart.disabled = false;
+      buttonStart.addEventListener('click', () => {
+        this.video.style.width = this.canvasWidth + "px";
+        this.video.style.height = this.canvasHeight + "px";
+        navigator.mediaDevices
+          .getDisplayMedia({ video: true, audio: false })
+          .then((stream) => {
+            buttonStart.disabled = true;
+            this.video.srcObject = stream;
+            this.video.play();
+            resolve();
+          });
+      });
+    });
+  }
+
+  blit(callback: BlobCallback) {
+    const context = this.canvas.getContext("2d");
+    this.canvas.width = this.canvasWidth;
+    this.canvas.height = this.canvasHeight;
+    context.drawImage(this.video, 0, 0, this.canvasWidth, this.canvasHeight);
+    this.canvas.toBlob(callback, this.imageFormat);
+  }
+}
+
+
+export class Producer {
+  timerId = null;
+  device: MediasoupClient.Device = new MediasoupClient.Device();
+  transport: MediasoupClient.types.Transport | null = null;
+  producer: MediasoupClient.types.DataProducer | null = null;
+  constructor(
+    public readonly sock: WebSocketJsonRpc,
+  ) {
+  }
+
+  async createTransport(rtpCap: MediasoupClient.types.RtpCapabilities) {
+    await this.device.load({ routerRtpCapabilities: rtpCap });
+
+    const params: any = await this.sock.sendRequestAsync(
+      "create-producer-transport",
+      {},
+    );
+    this.transport = this.device.createSendTransport(params);
+
+    // connectイベントが発生したらパラメータを送信してサーバー側でWebRtcTransport.connect()を実行する
+    this.transport.on(
+      "connect",
+      async ({ dtlsParameters }, callback, errback) => {
+        console.log('transport.connect');
+        this.sock.sendRequestAsync("connect-producer-transport", {
+          transportId: this.transport.id,
+          dtlsParameters: dtlsParameters,
+        })
+          .then(callback)
+          .catch(errback);
+      },
+    );
+
+    // producedataイベントが発生したらパラメータを送信してサーバー側でDataProducerを生成する
+    this.transport.on("producedata", async (parameters, callback, errback) => {
+      console.log('transport.producedata');
+      try {
+        const id = await this.sock.sendRequestAsync<string>("produce-data", {
+          transportId: this.transport.id,
+          produceParameters: parameters,
+        });
+        callback({ id: id });
+      } catch (err) {
+        errback(err);
+      }
+    });
+  }
+
+  // Transportからデータ送信用のDataProducerを生成する
+  async createProducer(videoCanvas: VideoCanvas, interval: number = 3000) {
+    this.producer = await this.transport.produceData();
+    console.log(this.producer);
+
+    // https://mediasoup.discourse.group/t/dataproducer-on-open-close-error-events/4068
+    console.log(`state: ${this.producer.readyState}`);
+    this.producer.on("open", () => {
+      console.log('producer.open');
+      const sendFrame = () => {
+        videoCanvas.blit((blob) => {
+          const reader = new FileReader();
+          reader.onloadend = () => {
+            // 画面共有の画像データを送信
+            console.log('producer.send');
+            this.producer.send(reader.result);
+          };
+          reader.readAsArrayBuffer(blob);
+        });
+      };
+      this.timerId = setInterval(sendFrame, interval);
+    });
+  }
+}
+
+
